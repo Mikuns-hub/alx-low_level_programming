18ac84c5b5eef471f5f99546457d0329a3e0af11
Ololade <babatuyiololade1004@gmail.com>
2023-08-11 22:01:09 -0700

    mh commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Program Entries
+ * Reuturn: 0 (Success)
+ */
+
+int main(void)
+{
+	int single = '0';
+	int doubles = '0';
+
+	for (doubles = '0'; doubles <= '9'; doubles++)
+	{
+		for (single = '0'; single <= '9'; single++)
+		{
+			if (!((single == doubles) || (doubles > single)))
+			{
+				putchar(doubles);
+				putchar(single);
+				if (!(single =='9' && doubles == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
