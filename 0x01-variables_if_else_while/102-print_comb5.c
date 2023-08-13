b0bbe3e438d8c0eab44d0c8ecb7a7b3e4af44588
Ololade <babatuyiololade1004@gmail.com>
2023-08-12 18:19:00 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - The beginning
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+	int b;
+	
+	for (c = 0; c < 100; c++)
+	{
+		for (b = 0; b < 100; b++)
+		{
+			if (c < b)
+			{
+				putchar((c / 10) + 48);
+				putchar((c % 10) + 48);
+				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (c != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
