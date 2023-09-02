099a2304ff760f45659c5e45a306922a1ae3bc61
Ololade <babatuyiololade1004@gmail.com>
2023-09-01 18:57:30 -0700

    C-static library task 1

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c o stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On erroe, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
